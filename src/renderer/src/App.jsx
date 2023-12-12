e078f858ed4fb2314e212529cc3d6b0b4ee6c936
merryfeby <merryfebyanti@gmail.com>
2023-12-12 17:26:56 +0700

    fix bug

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,11 +1,11 @@
-import { Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
 export default function App() {
   return (
     <div style={{ display: 'flex' }}>
       <div style={{ flex: 1 }}>
         <ul>
-          <Navlink to='/222180563'>Febyy</Navlink>
+          <NavLink to='/222180563'>Febyy</NavLink>
         </ul>
       </div>
       <div style={{ flex: 7 }}>
