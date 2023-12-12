c88780ad1ea12155c4482d6a568b1a6c1972110e
evanks-stts <63597963+evanks-stts@users.noreply.github.com>
2023-12-12 16:36:47 +0700

    add router and new layout

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './assets/index.css'
 import App from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: []
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
